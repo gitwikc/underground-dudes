6f3f7340d14827450d3f1f169874a274244a06a2
gitwikc <sahusattwik@gmail.com>
2022-09-18 10:19:33 +0000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import * as React from "react";
+
+const App = () => {
+  const [username, setUsername] = React.useState(null);
+
+  return (
+    <div className="App">
+      <div className="interaction">
+        <div className="username">USER = {username || "<type username>"}</div>
+        <input
+          type="text"
+          value={username}
+          onChange={(e) => {
+            setUsername(e.target.value);
+          }}
+        />
+        <button
+          onClick={(e) => {
+            e.preventDefault();
+            console.log(`Welcome to underground dudes, ${username}!`);
+          }}
+        >
+          Lezzgo!!
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default App;
